14848efa7dacf97a778197c13fa8020bd73ad12b
Wdenberg Modesto <wdenberg@gmail.com>
2022-01-29 20:27:20 -0300

    Criado as variaveis e dados de entrada

--- src/Program/WmSystem.java
+++ src/Program/WmSystem.java
@@ -1,9 +1,20 @@
 package Program;
 
+import java.util.Scanner;
+
 public class WmSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int codigo, quantidade;
+		float valorPago;
+		
+		System.out.println("Qual o código do produto: ");
+		codigo = scan.nextInt();
+		System.out.println("Qual a quantidade do produto: ");
+		
 
 	}
 
